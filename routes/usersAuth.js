0897f1efce3fe99e37b594214ed3fc709612a746
Darlan Mendes <dedsondarlan@gmail.com>
2023-02-26 23:02:53 -0300

    Adição de rota login userauth

--- routes/usersAuth.js
+++ routes/usersAuth.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 const bcrypt = require('bcrypt')
-
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 // Seguindo a lógica o usuário deverá criar um perfil com usuário e senha esta que deverá ser criptografada
 
 router.post("/signup", (req,res)=>{
+    //nesta linha são armazenados valores vindos do corpo da requisição 
     let {user,password,email}= req.body
-    
+    //nesta linha são conferidos se existe usuario, senha e email válidos
     if(user&&password.length>6&&email.includes('@')){
         //depois de verificado se existe usuario e password válido é feita consulta no banco se o usuario já existe
         
@@ -16,17 +18,18 @@ router.post("/signup", (req,res)=>{
                 const usersAuth = JSON.parse(data)
                     
                 
-                const userAlreadyExisted = usersAuth.filter(userAuth=>userAuth.email === email)
+                const userAlreadyExists = usersAuth.filter(userAuth=>userAuth.email === email)
                 //se o usuário existir um aviso deve ser retornado
                 
-                if(userAlreadyExisted.length>0){
+                if(userAlreadyExists.length>0){
+                    //o array userAlreadyExists é maior que 0 quando existir usuario
                     res.status(422)
                     res.send({"erro":"Usuario já existente"})
                 }else{
-                    //Se o usuário já não existir então é permitido o cadastrado do usuário
+                    //Se o usuário  não existir então é permitido o cadastrado do usuário
                    
                     //a senha deve ser criptografada / utilizado o bcrypt que solicita uma função async
-                    let passwordHashed= await bcrypt.hash(password, 8)
+                    let passwordHashed= await bcrypt.hash(password,eval(process.env.SALT_HASH))
                     
                     const newUser = {
                         "user":user,
@@ -36,6 +39,7 @@ router.post("/signup", (req,res)=>{
                    usersAuth.push(newUser)
                    try{
                     fs.writeFileSync('./data/usersAuth.json', JSON.stringify(usersAuth) )
+                    res.status(201)
                     res.send({"user":newUser.user,"email":newUser.email})
                    }catch(error){
                     res.status(500)
@@ -56,6 +60,44 @@ router.post("/signup", (req,res)=>{
         res.send({"erro":"campos incompletos"})
     }
 })
+router.post('/login',(req,res)=>{
+    let {email, password}= req.body
+   
+    if(email.length>0&&password.length>6){
+        fs.readFile('./data/usersAuth.json',async (err,data)=>{
+            try{
+               const usersAuth = JSON.parse(data)
+
+               let userToBeVerified = usersAuth.filter((user)=>user.email === email)
+               if(userToBeVerified.length>0){
+                // criptografar senha enviada do usuario para comparar com a do banco
+                await bcrypt.compare(password, userToBeVerified[0].password,(err,result)=>{
+                    if(result){
+                        //avisar que a senha é valida 200 
+                        const token = jwt.sign(
+                            {"user":userToBeVerified.user, "email":userToBeVerified.email},
+                            process.env.JWT_SECRET,
+                            {expiresIn: '1h'}
+                            )
+                        res.send({"auth":true, "token":token})    
+                    }else{
+                        res.status(401)
+                        res.send({"erro":err})
+                    }
+                })
+                
+               }
+            }catch{
+              res.status(500)
+              res.send({"erro":"ocorreu um erro na verifição, tente novamente"})  
+            }
+        })
+    }else{
+        res.status(422)
+        res.send({"erro":"Todos os campos devem estar completos"+ err})
+    }
+})
+
 
 
 
