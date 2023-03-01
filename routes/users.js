a2a06387cded7873750807c5ad896a14585fb0e1
Jose Antonio <antoniojaraujo0@gmail.com>
2023-03-01 19:42:11 -0300

    função para atualização de dados de usuário(PUT e PATCH)

--- routes/users.js
+++ routes/users.js
@@ -14,9 +14,32 @@ router.post("/", function (req, res, next) {
 	res.send("Criar novo usuario");
 });
 
-router.patch("/:id", function (req, res, next) {
-	res.send("edita um usuario");
-});
+// Função para atualizar dados de usuário. PUT e PATCH mesmo resultado apos testes, por isso associei a função "update" aos dois métodos.
+function update(req, res, next) {
+	fs.readFile('./data/users.json', 'utf-8', (err, data) => {
+		if (err) {
+			return res.status(500).send('Erro ao ler arquivo de usuário.');
+		}
+
+		const users = JSON.parse(data)
+		const id = req.params.id
+
+		const updatedUser = users.find((user) => user.id === id)
+		if(!updatedUser) {
+			return res.status(404).send('Não foi encontrado o usuário.');
+		}
+
+		//retorna updatedUser atualizado com dados passados no body da requisição
+		Object.assign(updatedUser, req.body)
+
+		//já com 'users' atualizado repopula-se o arquivo com os novos dados
+		fs.writeFileSync('./data/users.json', JSON.stringify(users))
+		res.send(updatedUser);
+	})
+}
+
+router.put("/:id", update)
+router.patch("/:id", update)
 
 router.delete("/:id", function (req, res, next) {
 	fs.readFile("./data/users.json", "utf-8", (err, data) => {
